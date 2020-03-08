f65b09a8f1e6a45ec007dee380711ffd981d6f6b
lee <xiaopeng_li@qingsongchou.com>
2020-03-08 20:22:35 +0800

    完成广告位的增删改查

---
+++ app/controller/ad/slider.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class SliderController extends Controller {
+    async index () {}
+}
+
+export default SliderController;
--- app/router.js
+++ app/router.js
@@ -7,12 +7,15 @@ module.exports = app => {
     const { router, controller } = app;
     router.prefix('/api');
 
-    //  直接调用的接口
+//  通用调用接口
     router.get('/', controller.index.index);
     router.get('/upload', controller.index.upload);
 
-    //  广告位管理接口
+//  广告位接口
+    //  广告位位置管理接口
     router.get('/ad/position', controller.ad.position.index)
     router.put('/ad/position', controller.ad.position.update)
     router.delete('/ad/position/:id', controller.ad.position.delete)
+    //  轮播图管理
+    router.get('/ad/slider', controller.ad.slider.index)
 };
